a189a67a57a2b518d447ab76411f7ba3c3ceade5
Emily Boudreaux <emily@boudreauxmail.com>
2024-08-26 16:05:39 +0000

    removed eos from main branch

--- src/eos/c_eos_interface_external.h
+++
@@ -1,6 +0,0 @@
-#ifndef c_eos_interface_external_h
-#define c_eos_interface_external_h
-
-#include "c_eos_interface_def.h"
-
-#endif
--- src/eos/c_eos_interface_lib.h
+++
@@ -1,9 +0,0 @@
-#ifndef c_eos_interface_lib_h
-#define c_eos_interface_lib_h
-
-#include "c_eos_interface_external.h"
-#include "c_eos_interface_def.h"
-
-#include <stdbool.h>
-
-#endif
