a07fa5f9cf3fe00e8cc4bfd66796372bb225a812
Emily Boudreaux <emily@boudreauxmail.com>
2024-08-27 16:43:41 +0000

    build(CompilerSettings.cmake): change libmesac from c to c++ to use gtest
    gtest works with c++ and ESTER is primarily c++, therefore the build system for libmesac is being changed to c++
    
    BREAKING CHANGE:

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -1,17 +1,20 @@
 # cmake/CompilerSettings.cmake
-set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_COMPILER g++)
+set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_Fortran_COMPILER gfortran)
 
 # Set C standard
-set(CMAKE_C_STANDARD 99)
-set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 # Set Fortran standard
 set(CMAKE_Fortran_STANDARD 90)
 set(CMAKE_Fortran_STANDARD_REQUIRED ON)
 
 # Common compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
 set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -fPIE")
 
 enable_language(Fortran)
+
+set_source_files_properties(${CMAKE_SOURCE_DIR}/src/*.c PROPERTIES LANGUAGE CXX)
